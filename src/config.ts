008db562ad88ce6253e6964d5ff990cb85470dfd
liuying <wocanbe@163.com>
2022-04-06 18:03:40 +0800

    add forticlient to blacklist

---
+++ src/config.ts
@@ -0,0 +1,56 @@
+type keyStr = {
+  [key:string]: string
+}
+export type blackItem = {
+  name: string,
+  mode?: 'full'|'reg', // 完全匹配/正则匹配
+  path: string,
+  auth: {
+    RunAtLoad?: boolean,
+    OnDemand?: boolean,
+    KeepAlive?: boolean,
+    SuccessfulExit?: boolean
+  } // 开机运行、后台运行、退出重启
+}
+export const items: keyStr = {
+  startup: '/Library/StartupItems/',
+  sStartup: '/System/Library/StartupItems/',
+  preferences: '/Library/Preferences/',
+  agents: '/Library/LaunchAgents/',
+  sAgents: '/System/Library/LaunchAgents/',
+  uAgents: process.env.HOME + '/Library/LaunchAgents/',
+  daemons: '/Library/LaunchDaemons/',
+  sDaemons: '/System/Library/LaunchDaemons/'
+}
+export const whiteReg: string[] = [
+  '^com\.apple\..*',
+  '^com\.openssh\..*',
+]
+export const whiteList: string[] = [
+  items.preferences + '.GlobalPreferences.plist',
+  items.preferences + 'org.cups.printers.plist',
+  items.sDaemons + 'bootps.plist',
+  items.sDaemons + 'com.vix.cron.plist',
+  items.sDaemons + 'org.apache.httpd.plist',
+  items.sDaemons + 'org.cups.cups-lpd.plist',
+  items.sDaemons + 'org.cups.cupsd.plist',
+  items.sDaemons + 'org.net-snmp.snmpd.plist',
+  items.sDaemons + 'org.openldap.slapd.plist',
+  items.sDaemons + 'ntalk.plist',
+  items.sDaemons + 'ssh.plist',
+  items.sDaemons + 'tftp.plist',
+  items.uAgents + 'com.google.keystone.agent.plist',
+  items.daemons + 'com.oracle.oss.mysql.mysqld.plist',
+  items.daemons + 'com.docker.vmnetd.plist'
+]
+export const blackList: blackItem[] = [
+  {
+    name: 'forticlient',
+    mode: 'reg',
+    path: '^com\.fortinet\.forticlient\..*',
+    auth: {
+      RunAtLoad: false,
+      KeepAlive: {}
+    }
+  }
+]
--- src/scan.ts
+++ src/scan.ts
@@ -1,50 +1,20 @@
-import { readdirSync, readFileSync, statSync } from 'fs'
-import { join } from 'path'
-import { plist2json } from 'plist2'
+import { readdirSync, readFileSync, writeFileSync, statSync } from 'fs'
+import { join, resolve } from 'path'
+import { parse, build } from 'plist'
+import { whiteList, whiteReg, items, blackList } from './config'
 
-const a = {
-  a: '/Library/StartupItems/',
-  b: '/System/Library/StartupItems/',
-  c: '/Library/Preferences/',
-  d: '/Library/LaunchAgents/',
-  e: '/System/Library/LaunchAgents/',
-  f: process.env.HOME + '/Library/LaunchAgents/',
-  g: '/Library/LaunchDaemons/',
-  h: '/System/Library/LaunchDaemons/'
-}
-const b: string[] = [
-  '^com\.apple\..*',
-  '^com\.openssh\..*',
-]
-const c: string[] = [
-  a.c + '.GlobalPreferences.plist',
-  a.c + 'org.cups.printers.plist',
-  a.h + 'bootps.plist',
-  a.h + 'com.vix.cron.plist',
-  a.h + 'org.apache.httpd.plist',
-  a.h + 'org.cups.cups-lpd.plist',
-  a.h + 'org.cups.cupsd.plist',
-  a.h + 'org.net-snmp.snmpd.plist',
-  a.h + 'org.openldap.slapd.plist',
-  a.h + 'ntalk.plist',
-  a.h + 'ssh.plist',
-  a.h + 'tftp.plist',
-  a.f + 'com.google.keystone.agent.plist',
-  a.g + 'com.oracle.oss.mysql.mysqld.plist',
-  a.g + 'com.docker.vmnetd.plist'
-]
-
-function d () {
+const fileCfg = { encoding: 'utf8' as BufferEncoding }
+function scan () {
   const flists:[string, string][] = []
-  for (const item in a) {
-    const dirPath = a[item]
+  for (const item in items) {
+    const dirPath = items[item]
     const list = readdirSync(dirPath)
     list.forEach(fileName => {
       const filePath = join(dirPath, fileName)
       const stats = statSync(filePath)
       if (stats.isFile()) {
-        if (!c.includes(filePath)) {
-          for (const regStr of b) {
+        if (!whiteList.includes(filePath)) {
+          for (const regStr of whiteReg) {
             const reg = new RegExp(regStr, 'i')
             if (reg.test(fileName)) return
           }
@@ -55,28 +25,94 @@ function d () {
   }
   return flists.sort((a, b) => a[0].localeCompare(b[0]))
 }
-export function check () {
-  const plists = d()
-  const autoRuns: string[] = []
-  for (const plist of plists) {
-    const dirPath = a[plist[1]]
-    const readPath = dirPath + plist[0]
-    const fileCfg = { encoding: 'utf8' as BufferEncoding }
-    try {
-      const plistData = readFileSync(readPath, fileCfg)
-      const jsonData = plist2json(plistData)
-      const plistCfg = JSON.parse(jsonData)
-      let isAutoRun = !1
-      if (plistCfg.RunAtLoad) isAutoRun = !0
-      if (plistCfg.OnDemand) isAutoRun = !0
-      if (plistCfg.KeepAlive) isAutoRun = !0
-      if (isAutoRun) {
-        // 为了避免问题，不在这儿做修改，请手动修改
-        autoRuns.push(readPath)
+function getRawType (val: unknown):string {
+  return Object.prototype.toString.call(val).slice(8, -1)
+}
+function isObj(obj:unknown):obj is object {
+  return getRawType(obj) === 'Object'
+}
+function isBool (val:unknown):val is boolean {
+  return val === true || val === false
+}
+function getAuth (plistName: string) {
+  let auth: any = {}
+  for (let i = 0; i < blackList.length; i++) {
+    const item = blackList[i]
+    const mode = item.mode || 'full'
+    if (mode === 'full') {
+      if (item.path === plistName) {
+        auth = item.auth
+        break
       }
-    } catch (e) {
-      console.log('read failure', readPath)
+    } else {
+      const reg = new RegExp(item.path, 'i')
+      if (reg.test(plistName)) {
+        auth = item.auth
+        break
+      }
     }
   }
-  console.log(autoRuns)
+  return auth
+}
+function checkPlist (readPath: string, plistName: string) {
+  let oldData: string, plistCfg: any
+  try {
+    const plistData = readFileSync(readPath, fileCfg)
+    plistCfg = parse(plistData) as any
+    oldData = JSON.stringify(plistCfg)
+  } catch (e) {
+    console.log('read failure', readPath)
+    return
+  }
+  const auth = getAuth(plistName)
+  let autoRun = false, isEdit = false
+  if (plistCfg.RunAtLoad) {
+    autoRun = true
+    if (auth.RunAtLoad === false) {
+      plistCfg.RunAtLoad = auth.RunAtLoad
+      isEdit = true
+    }
+  }
+  if (plistCfg.OnDemand) {
+    autoRun = true
+    if (auth.OnDemand === false) {
+      plistCfg.OnDemand = auth.OnDemand
+      isEdit = true
+    }
+  }
+  if (plistCfg.KeepAlive===true) {
+    autoRun = true
+    if (auth.KeepAlive===false) {
+      plistCfg.KeepAlive = auth.KeepAlive
+      isEdit = true
+    }
+  } else if (isObj(plistCfg.KeepAlive)) {
+    autoRun = true
+    if (auth.SuccessfulExit === false && isBool(plistCfg.KeepAlive.SuccessfulExit)) {
+      delete plistCfg.KeepAlive.SuccessfulExit
+      isEdit = true
+      if (JSON.stringify(delete plistCfg.KeepAlive) === '{}') delete plistCfg.KeepAlive
+    }
+  }
+  if (autoRun) console.log(readPath)
+  if (isEdit) {
+    const newData = build(plistCfg)
+    return { oldData, newData }
+  }
+}
+function writeToFile (backPath: string, oldData: string, readPath:string, newData: string) {
+  console.log(`backup ${backPath}`)
+  writeFileSync(resolve('backup/' + backPath + '.json'), oldData, fileCfg)
+  writeFileSync(readPath, newData, fileCfg)
+}
+export function check () {
+  const plists = scan()
+  for (const plist of plists) {
+    const readPath = items[plist[1]] + plist[0]
+    const datas = checkPlist(readPath, plist[0])
+    if (datas) {
+      const backPath = plist[1] + '/' + plist[0].slice(0, -5) + 'json'
+      writeToFile(backPath, datas.oldData, readPath, datas.newData)
+    }
+  }
 }
